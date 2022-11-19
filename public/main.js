9b73db56f87228cf3df4a0bfa1c8a1bef9fa176a
blazeeebit <blazeeebit@gmail.com>
2022-11-19 23:42:56 +0500

    Minecraft

--- public/main.js
+++ public/main.js
@@ -19,15 +19,12 @@ const createWindow = () => {
         console.log("Error")
     })
 }
-
 app.on('ready', createWindow)
-
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
 })
-
 app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
